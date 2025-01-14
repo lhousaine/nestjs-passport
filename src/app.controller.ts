c7823eaf86d826981d0798c9f15636eea11c9183
lhouarhou <l.ouarhou.ensa@gmail.com>
2025-01-14 17:27:11 +0100

    feat: init the project

---
+++ src/app.controller.ts
@@ -0,0 +1,43 @@
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import { AppService } from './app.service';
+import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { Public } from './auth/utilities';
+
+@Controller()
+export class AppController {
+  constructor(
+    private readonly appService: AppService,
+    private readonly authService: AuthService,
+  ) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/logout')
+  async logout(@Request() req) {
+    return req.logout();
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
+
+  @Public()
+  @Get('pub')
+  findAll() {
+    return [];
+  }
+}
